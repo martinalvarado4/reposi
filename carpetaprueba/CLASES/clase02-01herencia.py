750778d6a5a12c01a59970b97b87644fafb36ee1
mealvarado <mealvarado@uc.cl>
2016-08-06 10:19:02 -0400

    herencia

---
+++ carpetaprueba/CLASES/clase02-01herencia.py
@@ -0,0 +1,13 @@
+### clase02-01"herencia"
+import sys
+while True:
+    print("hola toña 1 o 0")
+    c=int(input("ingrese opcion:"))
+    if c==1:
+        print("tomas gonzalez GANA")
+        break
+    elif c==0:
+        print("tomas gonzalez pierde por feo")
+        break
+    else:
+        print("ingresa 1 o 0 fea")
